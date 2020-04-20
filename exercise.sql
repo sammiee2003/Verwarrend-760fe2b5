59e63851b321f4b7585471ab73bae3d067902f27
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-20 10:02:57 +0200

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -5,14 +5,18 @@ USE sterrenstelsel;
 /*SELECT * FROM planeten;*/
 TRUNCATE TABLE  planeten;
 
-ALTER TABLE planeten ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+-- ALTER TABLE planeten ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
 ALTER TABLE planeten add COLUMN diameter INT;
 ALTER TABLE planeten add COLUMN afstand_tot_de_zon INT;
 ALTER TABLE planeten add COLUMN massa INT;
 ALTER TABLE planeten add COLUMN bezoek_datum DATE NULL;
-INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Zon", 0, 332.946, 1.392000);
-INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mercurius", 57.910000, 0.1, 4.880);
-INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Venus", 108.208930, 0.9, 12.104);
-INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Aarde", 149.597870, 1, 12.756);
-INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mars", 227.936640, 0.1, 6.794);
+INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Zon", 0, 332.946, 1.392000, null);
+INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mercurius", 57.910000, 0.1, 4.880, null);
+INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Venus", 108.208930, 0.9, 12.104, "1962-03-01");
+INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Aarde", 149.597870, 1, 12.756, null);
+INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mars", 227.936640, 0.1, 6.794, null);
+INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mars", 227.936640, 0.1, 6.794, null);
+update planeten
+set naam = 'mars', naam = 'teenalp'
+where id = 6;
 SELECT * FROM planeten;
